6e4dc27a349e890d0224d085bbf35de28cdbc8d2
kabdulla <mykafayahabdullahi@gmail.com>
2026-08-31 15:11:53 +0100

    calculator

---
+++ simple_calculator.py
@@ -0,0 +1,14 @@
+# Ask the user for two numbers and display:
+# Addition
+# Subtraction
+# Multiplication
+# Division
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
