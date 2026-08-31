40491cc05508750775559c5b02be4cc31231735e
kabdulla <mykafayahabdullahi@gmail.com>
2026-08-31 14:37:12 +0100

    login introduction

---
+++ personal_introduction.py
@@ -0,0 +1,8 @@
+name = input("Name: ")
+age = int(input("Age: "))
+state = input("State: ")
+occupation = input("Occupation: ")
+hobby = input("Hobby: ")
+
+
+print(f"Hello, my name is {name} . I am {age} years old and i am an indigene of  {state}. I am an {occupation} and i love making {hobby}.  ")
