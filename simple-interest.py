23f6c0d98c8680e017c0abfc559350dd6fd8d968
kabdulla <mykafayahabdullahi@gmail.com>
2026-09-01 15:43:58 +0100

    simple-interest

---
+++ simple-interest.py
@@ -0,0 +1,6 @@
+principal = float(input("principal($): "))
+rate = float(input("Rate(% per year): "))
+time = float(input("Time(years): "))
+SI = principal * rate * time / 100
+print(SI)
+print("----------")
