a47d3d8332f7764f8a53ad3f6c2f7f754b0e9730
kabdulla <mykafayahabdullahi@gmail.com>
2026-09-01 13:57:01 +0100

    celsius-fahrenheit convert

--- agecalculator.py
+++ agecalculator.py
@@ -4,3 +4,17 @@ birth_year = int(input("birth year: "))
 current_year = datetime.now().year
 approximate_age = current_year - birth_year
 print(approximate_age)
+
+#A more sophisticated one
+from datetime import datetime
+try:
+    birth_year = int(input("birth year: "))
+
+except ValueError:
+    print("Please enter a valid year (numbers only).")  
+
+else:
+
+    current_year = datetime.now().year
+    approximate_age = current_year - birth_year
+    print(f"your approximate age is {approximate_age}.")
---
+++ celsius-fahrenheit.py
@@ -0,0 +1,3 @@
+celsius = float(input("celsius:  "))
+fahrenheit = (celsius * 9/5) + 32
+print(f"Fahrenheit: {fahrenheit}")
