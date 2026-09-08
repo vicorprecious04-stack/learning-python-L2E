2e7902224df1c6be48ae8d6e5d1f0c67e6b3dace
kabdulla <mykafayahabdullahi@gmail.com>
2026-09-08 11:22:18 +0100

    student - information

---
+++ student-info.py
@@ -0,0 +1,22 @@
+#Student Information Program
+# Write a student information program requesting for the following:
+
+# Ask the user for:
+
+# Name
+# Age
+# State
+# School
+# Class
+# Favourite subject
+# Hobby
+
+
+Name = input("Name: ")
+Age = int(input("Age: "))
+State = input("State: ")
+School = input("School: ")
+Class = input("Class: ")
+Favourite_subject = input("Favourite subject: ")
+Hobby = input("Hobby: ")
+print(f"Hello, my name is {Name}. I am {Age} years old, live in  {State}, go to University of {School}, and I am in {Class}. My favourite subject is {Favourite_subject}, and my hobby is {Hobby}.")
